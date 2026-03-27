7865fd9ac68aee3dc772e30d015c7173efa27da3
Eusebio Manriquez <eusemac@editorial.cl>
2026-03-27 09:53:48 -0300

    fix: incorrect import path in BlockedAccount

--- src/pages/public/BlockedAccount.jsx
+++ src/pages/public/BlockedAccount.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { LogOut, ShieldAlert, Mail } from 'lucide-react'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from '../../context/AuthContext'
 import { useNavigate } from 'react-router-dom'
 
 export default function BlockedAccount() {
